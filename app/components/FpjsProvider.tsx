de84c6cf3bbfe6d73aa44eeb39b81a9104d93021
kodaneflash <jamesonmarcher@gmail.com>
2024-05-08 01:16:06 -0400

    ee

--- app/components/FpjsProvider.tsx
+++ app/components/FpjsProvider.tsx
@@ -1,16 +1,26 @@
 'use client';
 
-import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
-import { PropsWithChildren } from 'react';
+import { FpjsProvider, useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
+import { PropsWithChildren, useEffect } from 'react';
 
 export function MyFpjsProvider({ children }: PropsWithChildren) {
+  const { isLoading, error, data, getData } = useVisitorData(
+    {extendedResult: true},
+    {immediate: true}
+  );
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      console.log('Visitor data:', data);
+    }
+  }, [isLoading, data]);
+
   return (
     <FpjsProvider
       loadOptions={{
-      	apiKey: '45Zv9bO7SOUZdfuq1EAo',
-    		endpoint: 'https://metrics.aibuzz.us',
+        apiKey: '45Zv9bO7SOUZdfuq1EAo',
+        endpoint: 'https://metrics.aibuzz.us',
         scriptUrlPattern: "https://metrics.aibuzz.us/web/v<version>/<apiKey>/loader_v<loaderVersion>.js"
-
       }}
     >
       {children}
